6337730888d2d802c1cbdabbb12578c53d6fdb9d
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-27 21:18:31 -0800

    Swap two Numbers

--- Java_Programs/swap_two_num.java
+++ Java_Programs/swap_two_num.java
@@ -9,11 +9,20 @@ public class Main
         System.out.println("a="+a+" "+"b="+b);
         
     }
+    public static void swapThirdVariable(int a, int b){
+    
+       int temp=a;
+            a=b;
+            b=temp;
+        System.out.println("with using Third variable a="+a+" "+"b="+b);
+        
+    }
 	public static void main(String[] args) {
 	    
 	    Scanner sc= new Scanner(System.in);
 	    int a=sc.nextInt();
 	    int b=sc.nextInt();
 		swap(a,b);
+		swapThirdVariable(a,b);
 	}
 }
